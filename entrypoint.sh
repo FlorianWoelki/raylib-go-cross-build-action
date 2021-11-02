31ec7be14e5e7267105cfdf4cf58307eb0eb909e
FlorianWoelki <FlorianWoelki@gmx.de>
2021-11-02 10:42:39 +0100

    fix: issue with if

--- entrypoint.sh
+++ entrypoint.sh
@@ -32,7 +32,7 @@ for ((i = 0; i < ${#targets[@]}; ++i)); do
   output="${release_path}/${repo_name}_${os}_${arch}"
   if [ "$os" == "windows" ]; then
     output="$output.exe"
-  done
+  fi
 
   echo "----> Building project for: $os/$arch ($output, $cc)"
   GOOS=$os GOARCH=$arch CC=$cc CGO_ENABLED=1 go build -o $output
